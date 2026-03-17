59718ff19762dbdeda3f93d8183d5165b8b1097c
Oswaldo Castaneda <oswaldodaniel123g@gmail.com>
2026-03-16 22:26:20 -0600

    Agrego implementación al método store de OfertaController
    Contiene:
    - Validación sobre los datos recibidos

--- app/Http/Controllers/OfertaController.php
+++ app/Http/Controllers/OfertaController.php
@@ -31,7 +31,23 @@ class OfertaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'titulo' => 'required|min:5|max:50',
+            'vigencia' => 'required|date|after:today',
+            'tienda' => 'required|min:5',
+            'precio_original' => 'required|numeric|min:0',
+            'precio_descuento' => 'required|numeric|min:0'
+        ]);
+
+        $oferta = new Oferta();
+        $oferta->titulo = $request->titulo;
+        $oferta->vigencia = $request->vigencia;
+        $oferta->tienda = $request->tienda;
+        $oferta->precio_original = $request->precio_original;
+        $oferta->precio_descuento = $request->precio_descuento;
+        $oferta->save();
+
+        return redirect()->route('ofertas.index');
     }
 
     /**
